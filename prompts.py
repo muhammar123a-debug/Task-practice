582ec19ac195bbdcecc43d0344e9da2cbd23692e
muhammar123a-debug <muhammar123a@gmail.com>
2025-09-21 17:29:48 +0500

    Add project

---
+++ prompts.py
@@ -0,0 +1,24 @@
+import os 
+from dotenv import load_dotenv
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+load_dotenv()
+llm = ChatGoogleGenerativeAI(model='gemini-1.5-flash', google_api_key=os.getenv("GOOGLE_API_KEY") )
+
+summary_prompt = PromptTemplate(
+    input_variables = ["text"],
+    template = "Summarize text this 2 lines: \n\n{text}"
+)
+summary_chain = LLMChain(llm=llm, prompt=summary_prompt)
+
+print("Summary:" , summary_chain.run("Artificial Intelligence is changing industries..."))
+
+translate_prompt = PromptTemplate(
+    input_Variable = ["text"],
+    template ="Translate this text English into Urdu: \n\n{text}"
+)
+translate_chain = LLMChain(llm=llm, prompt=translate_prompt)
+print("Urdu:", translate_chain.run("Knowledge is power."))
+
